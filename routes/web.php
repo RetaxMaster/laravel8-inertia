a4eeb1fe1a967630aec68f5d2f96fcaed78ef3b4
RetaxMaster <retaxmaster@hotmail.com>
2021-01-26 16:46:21 -0600

    Creacion de rutas y algunos controladores

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\PageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -15,15 +17,20 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+}); */
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::view("/", "index");
+
+Route::get("dashboard", [PageController::class, "dashboard"])
+        ->middleware("auth:sanctum")
+        ->name("dashboard");
+
+Route::resource("notes", NoteController::class)
+        ->middleware("auth:sanctum");
